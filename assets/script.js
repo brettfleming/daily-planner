313acea650e09aa67806b74dbec5e2dc82a61150
Brett Fleming <brettfleming015@gmail.com>
2021-06-21 22:47:38 -0500

    added the if statement that changes the class of the time blocks based on the time of day

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,34 @@
-const day = [ 
-    {hour: "09Am"}, {hour: "10Am"}, {hour: "11Am"}, {hour: "12Am"}, {hour: "01pm"}, {hour: "02pm"},  {hour: "03pm"}, {hour: "04pm"}, {hour: "05pm"}
-]
-var today = moment();
+
+let today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do YYYY"))
 
-day.forEach(function(currentHour) {
+const day = [ 
+    {hour: "9Am"}, {hour: "10Am"}, {hour: "11Am"}, {hour: "12Am"}, {hour: "1pm"}, {hour: "2pm"},  {hour: "3pm"}, {hour: "4pm"}, {hour: "5pm"}
+]
+day.forEach(function(today) {
     let row = $("<div>").addClass("row");
-    let hourField = $("<div>").text(`${currentHour.hour}`).addClass("hour");
-    let reminder = $("<div>").addClass("description");
-    let data = $("<textarea>");
-    var saveButton = $("<h3> Save </h3>")
-    var save = $("<button>").addClass("saveBtn");
-
-
+    let timeblock = $("<div>").text(`${today.hour}`).addClass("hour");
+    let reminder = $("<div>").addClass(" col-md-8 description");
+    let data = $("<textarea>").addClass("col-md-12");
+    let saveButton = $("<h3> Save </h3>")
+    let save = $("<button>").addClass("saveBtn");
+   
     $(".container").append(row);
     save.append(saveButton); 
     reminder.append(data);
-    row.append(hourField, reminder, save);
-    
+    row.append(timeblock);
+    row.append(reminder);
+    row.append(save);
+
+     // this if statement changes the classes on the timeblocks to past present and future
+     if (today.time < moment().format("HH")) {
+        data.addClass("past");
+    } else if (today.time === moment().format("HH")) {
+        data.addClass("present");
+    } else {
+        data.addClass("future");
+    }
 });
+
+
 
