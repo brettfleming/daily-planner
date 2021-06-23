57a700a739d96deca95b2fab12b4e44f9310103a
Brett Fleming <brettfleming015@gmail.com>
2021-06-22 19:56:51 -0500

    finished the readme file, and fixed some bugs with the class switching function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hw5-day-planner
+
+## Task 
+    The Task for this assignment was to make a day planner the dynamically update as the day goes on. The Time blocks need to change color based on the time, it should have three colors one for past, present and future. lastly the reminder should save to local stroage so that each time they load the page it populates. 
+
+
+## The Project
+    For this project I used Jquery to select and dynamically generate the day planner which has a structure similar to a list. The planner is made up of the Time Block section, reminder section, and lastly the save button. The Time block shows the time of day in which the reminder is for, the reminder is where the reminder will be typed and saved when the save button is clicked. The save function is a work in progress right now, I will hopefully figure it out soon.
+    
+    This project could be used by someone to keep track of things the have schduled for the day during work hours. This is a simple project that has a lot of limitation on what it can actully be used for. 
--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,30 @@ let today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do YYYY"))
 
 const day = [ 
-    {hour: "9Am"}, {hour: "10Am"}, {hour: "11Am"}, {hour: "12Am"}, {hour: "1pm"}, {hour: "2pm"},  {hour: "3pm"}, {hour: "4pm"}, {hour: "5pm"}
+    {hour: "9Am", hourNumber: 9}, {hour: "10Am", hourNumber: 10}, {hour: "11Am", hourNumber: 11}, {hour: "12Am", hourNumber: 12}, {hour: "1pm", hourNumber: 13},
+     {hour: "2pm", hourNumber: 14},  {hour: "3pm", hourNumber: 15}, {hour: "4pm", hourNumber: 16}, {hour: "5pm", hourNumber: 17}
 ]
+const timeOfDay = [
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17"
+]
+
 day.forEach(function(today) {
     let row = $("<div>").addClass("row");
-    let timeblock = $("<div>").text(`${today.hour}`).addClass("hour");
-    let reminder = $("<div>").addClass(" col-md-8 description");
+    let hourClass = getHourClass(today.hourNumber)
+    let timeblock = $("<div>").text(`${today.hour}`).addClass("hour")
+    let reminder = $("<div>").addClass(" col-md-8 description").addClass(hourClass);
     let data = $("<textarea>").addClass("col-md-12");
     let saveButton = $("<h3> Save </h3>")
     let save = $("<button>").addClass("saveBtn");
-   
+   //appending all the elements together to give the planner its list like structure
     $(".container").append(row);
     save.append(saveButton); 
     reminder.append(data);
@@ -21,14 +35,29 @@ day.forEach(function(today) {
     row.append(save);
 
      // this if statement changes the classes on the timeblocks to past present and future
-     if (today.time < moment().format("HH")) {
-        data.addClass("past");
-    } else if (today.time === moment().format("HH")) {
-        data.addClass("present");
-    } else {
-        data.addClass("future");
-    }
+     
 });
 
+function getHourClass(hourNumber){
+    if (hourNumber < parseInt(moment().format("HH"))) {
+       return "past";
+    } else if (hourNumber === parseInt(moment().format("HH"))) {
+       return "present"
+    } else if (hourNumber > parseInt(moment().format("HH") ))  {
+       return "future"
+    }
+}
 
-
+// for (let i = 0; i < timeOfDay.length; i++) {
+//     let data = $("textarea")
+//     console.log(parseInt(timeOfDay[i]) + '|' + parseInt(moment().format("HH")) ) 
+//     if (parseInt(timeOfDay[i]) < parseInt(moment().format("HH"))) {
+//         data.addClass("past");
+//     } else if (parseInt(timeOfDay[i]) === parseInt(moment().format("HH"))) {
+//         data.addClass("present");
+//     } else if (parseInt(timeOfDay[i]) > parseInt(moment().format("HH") ))  {
+//         data.addClass("future");
+//     }
+//     console.log(timeOfDay[i]);
+// }
+// console.log(moment().format("HH"));
